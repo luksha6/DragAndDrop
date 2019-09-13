2a1a16e91abc1c3caf872f04cd3f892b138b9515
Luka <l.bjelica@globalsoft.ba>
2019-09-13 10:05:12 +0200

    Remove background change on drag start

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 function onDragStart(event) {
     event.dataTransfer.setData('text/plain', event.target.id);
-    event.currentTarget.style.backgroundColor = 'lightcoral';
         console.log("Drag Start!");
 }
 
