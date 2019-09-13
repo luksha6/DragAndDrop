5189fbf4c5896a6ee0276a7ff0338abea917d3d1
Luka <l.bjelica@globalsoft.ba>
2019-09-13 10:02:58 +0200

    playing with drag and drop (practice)

---
+++ app.js
@@ -0,0 +1,19 @@
+function onDragStart(event) {
+    event.dataTransfer.setData('text/plain', event.target.id);
+    event.currentTarget.style.backgroundColor = 'lightcoral';
+        console.log("Drag Start!");
+}
+
+function onDragFinish(event) {
+    console.log("Drag Finish!");
+    event.preventDefault();
+}
+
+function onDrop(event) {
+    console.log("Drop");
+    const id = event.dataTransfer.getData('text');
+    const draggableElement = document.getElementById(id);
+    const dropzone = event.target;
+    dropzone.appendChild(draggableElement);
+    event.dataTransfer.clearData();
+}
